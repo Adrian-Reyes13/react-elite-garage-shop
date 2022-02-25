5c57c1fa80d7b4543f67afffcdedcfee9b47494a
Adrian-Reyes13 <99783541+Adrian-Reyes13@users.noreply.github.com>
2022-02-25 05:56:05 -0500

    Add files via upload

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+const Header = () => {
+    return(
+        <header> 
+            <h1> Menú </h1> 
+            <nav>
+                <a href="#"> Inicio </a>
+                <a href="#"> Productos </a>
+                <a href="#"> Sobre nosotros </a>
+            </nav>            
+        </header>
+
+    )
+}
